1a24f944392e948caededcdf00ef0d6a31414fea
chsponciano <chs.ponciano@gmail.com>
2019-11-23 18:38:27 -0300

    implementação do KNN analise

--- demoD1.py
+++ demoD1.py
@@ -1,34 +1,58 @@
-import utils
+import numpy as np
+import scipy.io as scipy
+import matplotlib.pyplot as plt
 from math import sqrt
+from scipy import stats
 
+def read() -> list:
+    return scipy.loadmat(f'data/grupoDados1.mat')
 
-def calculate_knn(grupoTrain : list, trainRots : list, grupoTest : list, k : int):
-    # %Para cada exemplo de teste
-    # % Calcule a distância entre o exemplo de teste e os dados de treinamento
-    _dist = dist(grupoTest)
+def knn(grupoTrain : np, trainRots : np, grupoTest : np, k : int) -> np:
+    _labels = np.zeros([grupoTest.shape[0], trainRots.shape[1]])
 
+    for i in range(grupoTest.shape[0]):
+        _copy_test  = np.tile(grupoTest[i,:], (grupoTrain.shape[0], 1))
+        _dists      = dist(_copy_test, grupoTrain)
+        _neighbors  = np.argsort(_dists)[:k]
+        _swat       = trainRots[_neighbors]       
+        _labels[i]  = _swat.sum(axis=0)
 
-    # % Ordene as distâncias. A ordem iX de cada elemento ordenado é
-    # importante:
-    # % [distOrdenada ind] = sort(...);
-    # % O rótulo previsto corresponde ao rótulo do exemplo mais próximo (iX(1))
-    pass
+    return _labels
 
-def dist(grupoTrain : list, grupoTest : list) -> float:
-    return sqrt(sum([(test - train) ** 2 for test, train in zip(grupoTest, grupoTrain)]))
+def accuracy(predicted_label : np, testRots : np) -> float:
+    return float(sum(predicted_label == testRots) / len(testRots))
 
-def normalization():
-    pass
+def dist(x : np, y : np) -> float:
+    return np.sqrt(np.power(x - y, 2).sum(axis=1))
 
-def plot():
-    pass
+def normalization(data : list, minmax : list) -> list:
+    return [(line[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0]) for i in range(len(data[0])) for line in data]
+        
 
-
+def plot(data : np, labels : np, d1 = 1, d2 = 2):
+    fig = plt.figure()
+    plt.suptitle(f'KNN')
+    plt.plot(data[labels == 1][d1], data[labels == 1][d2], color='red', marker='^')
+    plt.plot(data[labels == 2][d1], data[labels == 2][d2], color='blue', marker='+')
+    plt.plot(data[labels == 3][d1], data[labels == 3][d2], color='green', marker='o')
+    
 if __name__ == "__main__":
-    _data_set   = utils.read(1)
-    grupoTrain  = _data_set['grupoTrain']
-    trainRots   = _data_set['trainRots']
-    grupoTest   = _data_set['grupoTest'] 
-    testRots    = _data_set['testRots']
+    _data_set    = read()
 
+    _grupoTrain  = np.asarray(_data_set['grupoTrain'])
+    _trainRots   = np.asarray(_data_set['trainRots'])
+    _grupoTest   = np.asarray(_data_set['grupoTest'])
+    _testRots    = np.asarray(_data_set['testRots'])
 
+    _predicted_label = knn(_grupoTrain, _trainRots, _grupoTest, 1)
+    _accuracy        = accuracy(_predicted_label, _testRots)
+    print(_accuracy)
+
+    assert (_accuracy ==  0.96), "The accuracy of K = 1 must be 96%."
+
+    _predicted_label = knn(_grupoTrain, _trainRots, _grupoTest, 10)
+    _accuracy        = accuracy(_predicted_label, _testRots)
+    
+    print(_accuracy)
+
+    assert (_accuracy ==  0.94), "The accuracy of K = 10 must be 94%."
--- utils.py
+++
@@ -1,5 +0,0 @@
-import scipy.io as scipy
-
-
-def read(id_data_set : int = 0) -> list:
-    return scipy.loadmat(f'data/grupoDados{id_data_set}.mat')
